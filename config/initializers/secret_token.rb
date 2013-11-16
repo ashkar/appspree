b6e8526774c8b4c019cb4ae7bf20c34f3ff4ebcc
ashkar <ashkar55555@gmail.com>
2013-11-16 10:32:03 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appspree::Application.config.secret_token = '21e1d3a770fff3ee1b11391fe238d20a28520b52504e71dfb1b304d68f85eb928a0ca03163315dea75137f949b39651d0ce72d3d66d874c754c552fe56bdaa9c'
